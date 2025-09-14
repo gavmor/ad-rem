9f507c46e2b180f04d285212f75467f2f07ea5e9
Gavin Morgan <hello@gavmor.com>
2025-09-14 11:56:04 -0700

    initial caporal

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import { program } from "@caporal/core"
+
+program
+  .argument("<name>", "Name to greet")
+  .action(({ logger,  args }: {logger: any, args:any}) => {
+    logger.info("Hello, %s!", args.name)
+  })
+
+program.run(pruneBunArtifacts(Bun.argv))
+
+
+function pruneBunArtifacts(args: string[]) {
+  return args.slice(2).filter(arg => !/ad-rem/.test(arg))
+}
