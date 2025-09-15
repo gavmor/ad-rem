45480670c7be1dcbaee5234edb255d09f32e455f
Gavin Morgan <hello@gavmor.com>
2025-09-15 12:36:18 -0700

    extract infer() placeholder

--- src/conclude.ts
+++ src/conclude.ts
@@ -1,22 +1,18 @@
 import path from "path";
 import type { FilePath } from "./validatePath";
 import fs from "fs";
+import { infer } from "./infer";
 
 export function conclude(sourceDir: FilePath, query: string) {
-    //  How might we lazily read the contents of all the files from a directory in Bun?
-    //  Or is it adequate to read them all into memory at once?
-    const files = fs.readdirSync(sourceDir)
-        .map(file => fs.readFileSync(path.join(sourceDir, file)));
-
-    return files.reduce(drawConclusion(query), "INSUFFICIENT DATA FOR MEANINGFUL ANSWER.");
+    return files(sourceDir).reduce(drawConclusion(query), "INSUFFICIENT DATA FOR MEANINGFUL ANSWER.");
 }
 
 const drawConclusion = (query: string) =>
     (conclusion: string, file: Buffer) =>
         infer(file, query, conclusion);
 
+const files = (sourceDir: string) => fs
+    .readdirSync(sourceDir)
+    .map(file => fs.readFileSync(path.join(sourceDir, file)));
 
-function infer(file: Buffer, query: string, soFar: string) {
-    // Placeholder for LLM inference logic
-    return `Inferred from ${file} with query "${query}" and so far "${soFar}"\n`;
-}
+
---
+++ src/infer.ts
@@ -0,0 +1,5 @@
+export function infer(newEntry: Buffer, query: string, extantConclusion: string) {
+    // Placeholder for LLM inference logic
+    return `Insofar as ${extantConclusion}, yet given "${newEntry}", "${query}"\n`;
+    return "foo";
+}
